38148df30758e15f7e0f8bdf95b8d188183cd6ad
Paweł Łęczycki <pawel@msd.info.pl>
2021-07-21 15:28:50 +0200

    StudentsService

---
+++ Services.Interfaces/IStudentsService.cs
@@ -0,0 +1,18 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces
+{
+    public interface IStudentsService
+    {
+        void Create(Student student); 
+        Student Read(int index);
+        IEnumerable<Student> Read();
+        bool Update(int index, Student student);
+        bool Delete(int index);
+    }
+}
---
+++ Services/StudentsService.cs
@@ -0,0 +1,69 @@
+﻿using Models;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace Services
+{
+    public class StudentsService : IStudentsService
+    {
+        //private ICollection<Student> _students;
+        private IList<Student> _students;
+
+        public StudentsService()
+        {
+            _students = new List<Student>();
+            Create(new Student("Ewa", "Ewowska", new DateTime(1990, 12, 23), 123987));
+            Create(new Student("Michał", "Michałowski", new DateTime(1995, 1, 3), 523768));
+        }
+
+        public void Create(Student student)
+        {
+            _students.Add(student);
+        }
+
+        public bool Delete(int index)
+        {
+            var student = Read(index);
+            if (student == null)
+                return false;
+            return _students.Remove(student);
+        }
+
+        public Student Read(int index)
+        {
+            //for (var i = 0; i < _students.Count; i++ /*i += 1*/ /*i = i + 1*/)
+            //{
+            //    var student = _students[i];
+            foreach (var student in _students)
+            {
+                if (student.Index == index)
+                    return student;
+            }
+
+            //int i = 0;
+            //while(i < _students.Count)
+            //{
+            //    var student = _students[i++];
+            //    if (student.Index == index)
+            //        return student;
+            //}
+
+            return null;
+        }
+
+        public IEnumerable<Student> Read()
+        {
+            return _students;
+        }
+
+        public bool Update(int index, Student student)
+        {
+            if (Delete(index))
+                return false;
+            student.Index = index;
+            Create(student);
+            return true;
+        }
+    }
+}
