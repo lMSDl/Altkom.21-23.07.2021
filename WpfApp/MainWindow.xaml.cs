33dd32f94c33544932c316c0e542b298e815698a
Paweł Łęczycki <pawel@msd.info.pl>
2021-07-23 14:02:50 +0200

    DataGrid

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Models;
+using Services;
+using Services.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +14,6 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfApp
@@ -18,11 +21,23 @@ namespace WpfApp
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+        }
+
+        IStudentsService Service = new StudentsService();
+        public ICollection<Student> Students { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            Students = Service.Read().ToList();
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Students)));
         }
     }
 }
